42bd5091fbed6d9d23bcfa73ea4fe59f468ffb15
notyusta <xabhista19@gmail.com>
2026-06-11 08:47:18 +0700

    Add webhost image: PHP 8.3, Node.js 22, Composer

---
+++ webhost/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+cd /home/container
+
+# Make internal Docker IP address available to processes.
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP
+
+# Print PHP & Node.js Version
+php -v
+node -v
+npm -v
+
+# Print Composer Version
+composer --version 2>/dev/null || echo "Composer not found"
+
+# Replace Startup Variables
+MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the Server
+eval ${MODIFIED_STARTUP}
